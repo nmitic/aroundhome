9332536be462300b4672941b447a9ddfcc401a5e
Nikola Mitic <nikolamitic@nikolas-mbp.localdomain>
2020-10-08 10:38:46 +0200

    added styles for picker skeleton

--- src/pages/TimeSlots/Picker/PickerSkeleton/PickerSkeleton.js
+++ src/pages/TimeSlots/Picker/PickerSkeleton/PickerSkeleton.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+import {Styled as PickerStyled} from '../Picker.styled.js';
+import {Styled} from './PickerSkeleton.styled';
 
 const PickerSkeleton = () => {
 
-  return null;
   return (
-    <h1>Skeleton goes here</h1>
+    <PickerStyled.Container>
+      <Styled.Name/>
+        <Styled.SelectedSlot/>
+        <PickerStyled.Listing>
+          <Fragment>
+            <Styled.Day/>
+            {
+              [1,2,3,4,5,6].map(index => <Styled.Item key={index}/>)
+            }
+            <Styled.Day/>
+            {
+              [1,2,3,4,5,6].map(index => <Styled.Item key={index}/>)
+            }
+          </Fragment>
+        </PickerStyled.Listing>
+    </PickerStyled.Container>
   )
 }
 
