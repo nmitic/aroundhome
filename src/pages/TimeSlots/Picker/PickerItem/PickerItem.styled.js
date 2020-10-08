6048d62391c2614f1d658a2852f49762b71230d4
Nikola Mitic <nikolamitic@nikolas-mbp.localdomain>
2020-10-08 09:01:02 +0200

    added styles for smaller screens and created ThemeProvider

--- src/pages/TimeSlots/Picker/PickerItem/PickerItem.styled.js
+++ src/pages/TimeSlots/Picker/PickerItem/PickerItem.styled.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 const Item = styled.div`
   cursor: pointer;
   margin-bottom: 1rem;
-  border: 1px solid gray;
+  border: 1px solid ${props => props.theme.colors.gray};
   max-width: 70%;
   margin-left: auto;
   margin-right: auto;
@@ -11,13 +11,13 @@ const Item = styled.div`
   border-radius: 5px;
 
   ${props => props.disabled && css`
-    background-color: beige;
+    background-color: ${props => props.theme.colors.disabled};;
     cursor: not-allowed;
   `}
 `;
 
 const Time = styled.time`
-  font-size: 1.3rem;
+  font-size: ${props => props.theme.fontSizes.h3};
 `;
 
 export const Styled = {
--- src/pages/TimeSlots/TimeSlots.styled.js
+++ src/pages/TimeSlots/TimeSlots.styled.js
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 const Container = styled.main`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr;
+
+  @media(min-width: ${props => props.theme.breakpoints.desktop}) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 `;
 
 export const Styled = {
