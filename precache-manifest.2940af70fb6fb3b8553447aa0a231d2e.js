924a5ff18529c3d2d30e516eed3d821b2bc495c1
Nikola Mitic <nikolamitic@nikolas-mbp.localdomain>
2020-10-08 11:30:52 +0200

    Updates

---
+++ precache-manifest.2940af70fb6fb3b8553447aa0a231d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e765cf7df5a33f5fee5a690d94cb9701",
+    "url": "/aroundhome/index.html"
+  },
+  {
+    "revision": "ac2dc7595d58e8888b6c",
+    "url": "/aroundhome/static/js/2.27fc4734.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/aroundhome/static/js/2.27fc4734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a16ee0f2ae9cd5e59c3",
+    "url": "/aroundhome/static/js/main.0bb5efc7.chunk.js"
+  },
+  {
+    "revision": "b2b4a29b9bd818ca9732",
+    "url": "/aroundhome/static/js/runtime-main.24636ed8.js"
+  }
+]);
